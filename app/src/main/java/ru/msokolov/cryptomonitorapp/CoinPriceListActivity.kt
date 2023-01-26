f31536750b1bfa158ab84efdeb15bd87b8d62a4d
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-26 12:03:15 +0300

    added files from old project, app does not work

---
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/CoinPriceListActivity.kt
@@ -0,0 +1,34 @@
+package ru.msokolov.cryptomonitorapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
+import ru.msokolov.cryptomonitorapp.adapters.CoinInfoAdapter
+import ru.msokolov.cryptomonitorapp.pojo.CoinPriceInfo
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_price_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+}
---
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/pojo/CoinInfoListOfData.kt
@@ -0,0 +1,10 @@
+package ru.msokolov.cryptomonitorapp.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinInfoListOfData (
+    @SerializedName("Data")
+    @Expose
+    val data: List<Datum>? = null
+)
