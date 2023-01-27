ed4bb1205b63c067d443cc57dd781bcc4eba251a
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-27 22:53:31 +0300

    refactored adapter

--- app/src/main/java/ru/msokolov/cryptomonitorapp/presentation/list/adapter/CoinInfoAdapter.kt
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/presentation/list/adapter/CoinInfoAdapter.kt
@@ -23,21 +23,54 @@ class CoinInfoAdapter(private val context: Context) :
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
         val coin = getItem(position)
         with(holder) {
-            with(coin) {
-                val symbolsTemplate = context.resources.getString(R.string.symbols_template)
-                val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
-                binding.tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
-                binding.tvPrice.text = price
-                binding.tvLastUpdate.text = String.format(lastUpdateTemplate, lastUpdate)
-                Picasso.get().load(imageUrl).into(binding.ivLogoCoin)
-                itemView.setOnClickListener {
-                    onCoinClickListener?.onCoinClick(this)
-                }
+            val formattedValues = getFormattedInfoFromCoin(coin)
+            updateUi(binding, coin, formattedValues)
+            loadImage(coin.imageUrl, binding)
+
+            itemView.setOnClickListener {
+                onCoinClickListener?.onCoinClick(coin)
             }
+
         }
+    }
+
+    private fun loadImage(imageUrl: String?, binding: ItemCoinInfoBinding) {
+        Picasso.get().load(imageUrl).into(binding.ivLogoCoin)
+    }
+
+    private fun getStringFromRes(context: Context, resId: Int): String {
+        return context.resources.getString(resId)
+    }
+
+    private fun updateUi(
+        binding: ItemCoinInfoBinding,
+        coin: CoinInfoEntity,
+        formattedValues: HashMap<String, String>
+    ) {
+        binding.tvPrice.text = coin.price
+        binding.tvSymbols.text = formattedValues[SYMBOLS_KEY]
+        binding.tvLastUpdate.text = formattedValues[LAST_UPDATE_KEY]
+    }
+
+    private fun getFormattedInfoFromCoin(coin: CoinInfoEntity): HashMap<String, String> {
+        val symbolsTemplate = getStringFromRes(context, R.string.symbols_template)
+        val lastUpdateTemplate = getStringFromRes(context, R.string.last_update_template)
+
+        val formattedSymbols = String.format(symbolsTemplate, coin.fromSymbol, coin.toSymbol)
+        val formattedLastUpdate = String.format(lastUpdateTemplate, coin.lastUpdate)
+        val formattedValues = hashMapOf<String, String>()
+        formattedValues[SYMBOLS_KEY] = formattedSymbols
+        formattedValues[LAST_UPDATE_KEY] = formattedLastUpdate
+
+        return formattedValues
     }
 
     interface OnCoinClickListener {
         fun onCoinClick(coinInfoEntity: CoinInfoEntity)
+    }
+
+    companion object {
+        private const val SYMBOLS_KEY = "SYMBOLS"
+        private const val LAST_UPDATE_KEY = "LAST_UPDATE"
     }
 }
