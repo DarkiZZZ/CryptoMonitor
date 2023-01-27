39461156e6966033c6fb115bf24a54867a4f90ea
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-27 23:36:15 +0300

    added support of navigate up

--- app/src/main/java/ru/msokolov/cryptomonitorapp/presentation/list/MainActivity.kt
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/presentation/list/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.msokolov.cryptomonitorapp.presentation.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import ru.msokolov.cryptomonitorapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
