aabe4f29e8441656a0075ae604208a01dd8c2eb4
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-29 18:42:41 +0300

    refactored api key query

--- app/src/main/java/ru/msokolov/cryptomonitorapp/data/network/ApiService.kt
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/data/network/ApiService.kt
@@ -9,20 +9,22 @@ interface ApiService {
 
     @GET("top/totalvolfull")
     suspend fun getTopCoinsInfo(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
     ): CoinNamesListDto
 
     @GET("pricemultifull")
     suspend fun getFullPriceList(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
         @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
     ): CoinInfoJsonContainerDto
 
     companion object {
         private const val QUERY_PARAM_API_KEY =
+            "api_key"
+        private const val API_KEY =
             "6cffdc987d403d61eac4764e4165a4e484e70af19d4a90af84e2f5d7179349fd"
         private const val QUERY_PARAM_LIMIT = "limit"
         private const val QUERY_PARAM_TO_SYMBOL = "tsym"
