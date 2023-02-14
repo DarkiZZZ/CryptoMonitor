42402fb4e2208f6d939c484dfff47f1c526dd9d5
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-14 17:33:24 +0300

    removed unused dependencies, removed provide methods from module

--- app/src/main/java/ru/msokolov/cryptomonitorapp/di/DomainModule.kt
+++ app/src/main/java/ru/msokolov/cryptomonitorapp/di/DomainModule.kt
@@ -2,12 +2,8 @@ package ru.msokolov.cryptomonitorapp.di
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import ru.msokolov.cryptomonitorapp.data.repository.CoinRepositoryImpl
 import ru.msokolov.cryptomonitorapp.domain2.CoinRepository
-import ru.msokolov.cryptomonitorapp.domain2.usecase.GetCoinInfoListUseCase
-import ru.msokolov.cryptomonitorapp.domain2.usecase.GetCoinInfoUseCase
-import ru.msokolov.cryptomonitorapp.domain2.usecase.LoadDataUseCase
 
 @Module
 interface DomainModule {
@@ -16,19 +12,4 @@ interface DomainModule {
     @ApplicationScope
     fun bindCoinRepository(impl: CoinRepositoryImpl): CoinRepository
 
-
-   /* @Provides
-    fun provideGetCoinInfoListUseCase(repository: CoinRepository): GetCoinInfoListUseCase{
-        return GetCoinInfoListUseCase(repository = repository)
-    }
-
-    @Provides
-    fun provideGetCoinInfoUseCase(repository: CoinRepository): GetCoinInfoUseCase{
-        return GetCoinInfoUseCase(repository = repository)
-    }
-
-    @Provides
-    fun provideLoadDataUseCase(repository: CoinRepository): LoadDataUseCase{
-        return LoadDataUseCase(repository = repository)
-    }*/
 }
